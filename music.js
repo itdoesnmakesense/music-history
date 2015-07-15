db5119a555018878fb779de4639ca5424031187a
Juan Rodriguez <juan.j.rodriguez2@gmail.com>
2015-07-14 20:22:02 -0500

    complete homework

--- music.js
+++ music.js
@@ -6,10 +6,11 @@ songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album Th
 songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
 songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
 
-
-
+songs.unshift("Love Is A Losing Game > by Amy Winehouse on the album Back to Black");
+songs.push("Ice Princess > by Azealia Banks on the album Broke With Expensive Taste");
+songs.splice();
 for (var i = 0; i <= songs.length; i++) {
-	var revision = songs[i].replace(">", "-"); 
+	var revision = songs[i].replace(">", "-").replace("*", "").replace("@", "").replace("(", "").replace("!", ""); 
 	var music = document.getElementById("songs-content");
 	music.innerHTML += "<p>" + revision + "</p>";
 }
